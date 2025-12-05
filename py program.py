2f6573aefeac98e6db5dd803b605bfe89607f258
mobassirmahin91-star <mobassirmahin91@gmail.com>
2025-12-05 19:01:51 +0600

    Add files via upload
    first step

--- py program.py
+++ py program.py
@@ -1,1 +1,5 @@
-print("yo")
+import socket  # https://docs.python.org/3/library/socket.html
+import sys
+from datetime import datetime
+import threading  # https://docs.python.org/3/library/threading.html
+from queue import Queue  # https://docs.python.org/3/library/queue.html
